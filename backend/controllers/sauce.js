ac68c6dd869fa537a64cdd8cbbe58f552f83beca
Arnaud293 <arnaudlt293@gmail.com>
2022-05-27 15:59:54 +0200

    delete img when we modify this

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -78,9 +78,25 @@ exports.modifySauce = (req, res, next) => {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié' }))
-        .catch(error => res.status(400).json({ error }));
+    if (sauceObject.imageUrl == null) {
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Objet modifié' }))
+            .catch(error => res.status(400).json({ error }));
+    }
+    else {
+        Sauce.findOne({ _id: req.params.id })
+            .then((sauce) => {
+                const filename = sauce.imageUrl.split("/images/")[1];
+
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet supprimé' }))
+                        .catch(error => res.status(400).json({ error }));
+
+                });
+            });
+    }
+
 }
 
 exports.deleteSauce = (req, res, next) => {
